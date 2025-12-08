1323acb0da1bff140097bc3243186b62eccc3b32
Anchal Lama <anchal.percoidit@gmail.com>
2025-12-08 11:42:25 +0545

    All changes

--- RedisCrudAPI/RedisCrudAPI/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RedisCrudAPI.Models
-{
-    public abstract class BaseEntity
-    {
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; }
-        public bool IsDeleted { get; set; } = false;
-    }
-}
